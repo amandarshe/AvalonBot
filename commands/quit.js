05ad0d4b248c2b9201a537acd761edbfb6f15f7f
Amanda She <arscuties@yahoo.com>
2020-08-22 01:27:34 -0400

    message

--- commands/quit.js
+++ commands/quit.js
@@ -2,10 +2,11 @@ module.exports = {
 	name: 'quit',
 	description: "quit command",
 	execute(message, args, client) {
+		const token = require('./botToken.js');
 		message.channel.send("Quitting game...");
 		message.channel.send("Game over. Type '!avalon' to play again!")
 		.then(m => client.destroy())
-		.then(() => client.login('NzM3MjM3MzA2NTY5Nzg1NTA0.Xx6bvw.nDZs7RI8PuxnvBUnJWsJKvnx-CE'));
+		.then(() => client.login(token.getBotToken()));
 
 	}
 
